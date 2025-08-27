f5e183389c2e8ba99df724b35231b66fa3d29ff0
babludoodhwala <146544512+babludoodhwala@users.noreply.github.com>
2025-08-28 00:38:20 +0800

    Change chatbot name to Agus, Add new greeting message and by message

---
+++ src/main/java/Agus.java
@@ -0,0 +1,6 @@
+public class Agus {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Agus\n what can I do for you?");
+        System.out.println( "Bye. Hope to see you again soon!");
+    }
+}
