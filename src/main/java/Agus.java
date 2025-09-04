bd06e382ea099722759b347ca1dd4ebd3046260f
sivanshno <sivanshno@users.noreply.github.com>
2025-09-04 14:33:25 +0800

    Add Echo function to echo user inputs

--- src/main/java/Agus.java
+++ src/main/java/Agus.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Agus {
+
+    public static void echo() {
+        String userInput = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!userInput.equals("bye")) {
+            userInput = scanner.nextLine();
+            if (!userInput.equals("bye")) {
+                System.out.println(userInput);
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello! I'm Agus\n what can I do for you?");
+        Agus.echo();
         System.out.println( "Bye. Hope to see you again soon!");
     }
 }
